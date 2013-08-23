1e6bbb8c856ca195bcd7ff2fae0886311d7d0f34
Cheat- <mvardenis@gmail.com>
2013-08-23 01:21:12 +0100

    Update bot.js

--- bin/bot.js
+++ bin/bot.js
@@ -761,7 +761,7 @@
 
     roomHelpCommand.prototype.functionality = function() {
       var msg1, msg2;
-      msg1 = "Welcome to the Dubstep Den! Create a playlist and populate it with songs from either YouTube or Soundcloud.  ";
+      msg1 = "Welcome to the All ~ Let's Dance ! ~ß [ENG/FRA]! Create a playlist and populate it with songs from either YouTube or Soundcloud.  ";
       msg1 += "Click the 'Join Waitlist' button and wait your turn to play music. Most electronic music allowed, type '/theme' for specifics.";
       msg2 = "Stay active while waiting to play your song or I'll remove you.  Play good quality music that hasn't been played recently (check room history).  ";
       msg2 += "Avoid over played artists like Skrillex. Ask a mod if you're unsure about your song choice";
